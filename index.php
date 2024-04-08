d7c49f9b905ae67a2b97380fa7b9ed08ea583a99
vašek chvátal <38693443+ventross1@users.noreply.github.com>
2024-04-08 08:22:13 +0200

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$temp = file_get_contents("temp.html");
-$data = [
-    'logo' => file_get_contents("logosmartcook.svg")
-];
-foreach ($data as $key => $value) {
-    $temp = str_replace("{{$key}}", $value, $temp);
-}
-echo $temp;
